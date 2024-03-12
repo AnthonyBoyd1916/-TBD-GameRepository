c1a9876a3146f72a46a07e6281b29c3ccf8f703f
AnthonyBoyd1916 <Boyd-A31@ulster.ac.uk>
2024-03-12 10:48:36 +0000

    Level Prefabs added, level scenes created

---
+++ Assets/_ProjectFIles/Scripts/Anthony's Scripts/StairTeleport.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairTeleport : MonoBehaviour
+{
+    [SerializeField] private GameObject stairdestination;
+    [SerializeField] private float offset = 3f;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var Telepos = stairdestination.transform.position;
+        Telepos.y -= offset;
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.position = Telepos;
+        }
+    }
+}
