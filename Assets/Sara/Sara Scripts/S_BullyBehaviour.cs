dfbd5257abab22ed480d1180e991783e26031055
NotTifako <boyd-s47@ulster.ac.uk>
2024-03-19 11:40:48 +0000

    Added L2 enemies & Pause Menu Prefab

--- Assets/Sara/Sara Scripts/S_BullyBehaviour.cs
+++ Assets/Sara/Sara Scripts/S_BullyBehaviour.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class S_BullyBehaviour : MonoBehaviour
 {
     [SerializeField] float bullySpeed = 4f; // Editable in inspector
+    [SerializeField] GameObject[] waypoints;
 
     private Vector3 originalPosition;
     private Vector2 playerPosition;
     private bool playerInRange;
+    private int currentWaypointIndex = 0;
 
     void Start()
     {
@@ -21,9 +23,10 @@ public class S_BullyBehaviour : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(this.transform.position, playerPosition, Time.fixedDeltaTime * bullySpeed); // Moves towards the player at full speed
         }
-        if (playerInRange == false && this.transform.position != originalPosition) // Is player not in their range AND the clown isn't at their orignial position
+        if (playerInRange == false) // Is player not in their range AND the clown isn't at their orignial position
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, originalPosition, Time.fixedDeltaTime * (bullySpeed / 2)); // Moves back to their original spot at half speed
+            //transform.position = Vector2.MoveTowards(this.transform.position, originalPosition, Time.fixedDeltaTime * (bullySpeed / 2)); // Moves back to their original spot at half speed
+            transform.position = Vector2.MoveTowards(this.transform.position, waypoints[currentWaypointIndex].transform.position, Time.fixedDeltaTime * (bullySpeed / 2));
         }
     }
 
@@ -33,6 +36,18 @@ public class S_BullyBehaviour : MonoBehaviour
         {
             playerInRange = true; // Set bool to true
             playerPosition = collision.transform.position; // Save the position of the player so the bully can rush their position
+        }
+
+        else if (collision.gameObject.tag == "Waypoint")
+        {
+            if (currentWaypointIndex == 0)
+            {
+                currentWaypointIndex = 1;
+            }
+            else if (currentWaypointIndex == 1)
+            {
+                currentWaypointIndex = 0;
+            }
         }
     }
 
---
+++ Assets/Scripts/Pause/S_QuitToMain.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class S_QuitToMain : MonoBehaviour
+{
+    Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnQuitToMain);
+    }
+
+    void OnQuitToMain()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MenuMain");
+    }
+}
