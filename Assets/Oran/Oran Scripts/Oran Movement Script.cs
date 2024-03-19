9bbb84dcbdf4226e6daeff472607287c9895cded
AnthonyBoyd1916 <Boyd-A31@ulster.ac.uk>
2024-03-19 13:01:42 +0000

    Obstacles Level 1

---
+++ Assets/Oran/Oran Scripts/Oran Movement Script.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OranMovementScript : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private float horizontal;
+    public SpriteRenderer spriteRenderer;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 10f;
+    //private bool isFacingRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (horizontal > 0f)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (horizontal < 0f)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    public void OnJump(/*InputAction.CallbackContext context*/)
+    {
+        if (IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        //if (/*context.canceled && */IsGrounded())
+        //{
+        //    rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        //}
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    //private void flip()
+    //{
+    //    isFacingRight = false;
+    //    Vector3 localScale = transform.localScale;
+    //    localScale.x *= -1f;
+    //    transform.localScale = localScale;
+    //}
+
+    public void OnMove(InputValue axis)
+    {
+        horizontal = axis.Get<float>();
+    }
+}
