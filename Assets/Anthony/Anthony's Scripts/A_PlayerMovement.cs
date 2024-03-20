2f0ed7d526745fae77a3ea359ec316f222cd3c37
AnthonyBoyd1916 <Boyd-A31@ulster.ac.uk>
2024-03-20 10:59:43 +0000

    Had to switch player controller, issue with torch controls

--- Assets/Anthony/Anthony's Scripts/A_PlayerMovement.cs
+++ Assets/Anthony/Anthony's Scripts/A_PlayerMovement.cs
@@ -8,7 +8,11 @@ public class A_PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 8f;
     [SerializeField] private float jppower = 10f;
     [SerializeField] private float jpfalloff = 0.5f;
-    [SerializeField] private Rigidbody2D rb;    
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    public SpriteRenderer spriteRenderer;
+    private bool onground;
 
     void Awake()
     {
@@ -17,8 +21,8 @@ public class A_PlayerMovement : MonoBehaviour
     void Update()
     {
         horz = Input.GetAxisRaw("Horizontal"); 
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Jump Calculations
+        if (Input.GetKeyDown(KeyCode.Space) && onground == true)
         {
             rb.velocity = new Vector2(rb.velocity.x, jppower);
         }
@@ -26,18 +30,47 @@ public class A_PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jpfalloff);
         }
-
-        if (Input.GetKeyDown(KeyCode.S))
+        //Fast Fall
+        if (Input.GetKeyDown(KeyCode.S) && onground == false)
         {
             rb.gravityScale = 8f;
         }
-        if (Input.GetKeyUp(KeyCode.S))
+        if (Input.GetKeyUp(KeyCode.S) && onground == true)
         {
             rb.gravityScale = 1f;
+        }
+        //Sprite Flip
+        if (horz > 0f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (horz < 0f)
+        {
+            spriteRenderer.flipX = true;
         }
     }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horz * speed, rb.velocity.y);
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onground = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onground = false;
+        }
+    }
+    /*private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }*/
 }
