d47f23496bfcf5991a9e06696a61faa8b0685b83
NotTifako <boyd-s47@ulster.ac.uk>
2024-04-09 15:26:33 +0100

    Added a post processing vignette to level 1
    Will add to the other levels soon

--- Assets/Sara/Sara Scripts/S_FearL1.cs
+++ Assets/Sara/Sara Scripts/S_FearL1.cs
@@ -2,48 +2,45 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
 
 public class S_FearL1 : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera m_Camera;
-    [SerializeField] float defaultOrthoSize;
+    float defaultOrthoSize = 6;
     [SerializeField] float fearIncreaseSpeed = 0.002f; // Editable in the inspector
     [SerializeField] float fearDecreaseAmount = 0.002f; // Editable in the inspector
-    [SerializeField] Image vignette;
+    [SerializeField] Volume globalVolume;
 
     private float fear;
-    private float fearOrthoSize = 9;
-    private Color vignetteColor;
-    private Color fearVignetteColor;
+    private float fearOrthoSize;
+    private float vignetteMin = 0.2f;
+    private float vignetteIntensity;
+    private float vignetteMax = 0.7f;
     S_TorchBehaviour torchBehaviour;
+    Vignette vignette;
 
     void Start()
     {
         m_Camera.m_Lens.OrthographicSize = defaultOrthoSize;    // Sets the current Ortho size to be default
-
         InvokeRepeating("FearIncreaseAmbient", 1.0f, 0.1f);     // Makes the fear increase or decrease every 0.1 seconds.
-        vignetteColor = vignette.GetComponent<Image>().color;   // Gets the component colour of the vignette
+
+        globalVolume.profile.TryGet(out vignette);
+        
+        //vignette = globalVolume.GetComponent<Vignette>();
 
         torchBehaviour = GetComponent<S_TorchBehaviour>(); // Gets the a reference to the script
+        fearOrthoSize = defaultOrthoSize;
+        vignette.intensity.value = vignetteMin;
+        vignetteIntensity = vignetteMin;
     }
 
     private void Update()
     {
-        if (fear < 0.25 && fear > 0)
+        if (fear > 0.2 && fear <= 0.7 && vignetteIntensity != vignetteMax)
         {
-            float colourAlphaNumber = fear / 2; // Sets it so for the first quarter of the fear meter, the opacity is only half of the fear
-            fearVignetteColor.a = colourAlphaNumber;
-            fearVignetteColor.r = 1f; fearVignetteColor.b = 1f; fearVignetteColor.g = colourAlphaNumber; // Sets each of the colours to 1 as there was an issue of them all turning to 0
-            vignette.GetComponent<Image>().color = Color.Lerp(vignetteColor, fearVignetteColor, 0.1f); // Attempts to smooth out the opacity change
-        }
-        else if (fear > 0.25)
-        {
-            float colourAlphaNumber = fear; // Sets it so that the opacity is equal to the fear level
-            fearVignetteColor.a = colourAlphaNumber;
-            fearVignetteColor.r = 1f; fearVignetteColor.b = 1f; fearVignetteColor.g = colourAlphaNumber; // Same as above, sets colour channels to 1
-            vignette.GetComponent<Image>().color = fearVignetteColor; //Color.Lerp(vignetteColor, fearVignetteColor, 0.1f); // Same as above, tries to smooth the change
+            vignette.intensity.value = fear;
         }
     }
 
@@ -58,7 +55,7 @@ public class S_FearL1 : MonoBehaviour
             }
 
             fear += fearIncreaseSpeed; // Increases the fear level
-            //Debug.Log(fear); // Delete after testing
+            Debug.Log(fear); // Delete after testing
         }
 
         else if (fear < 1.2 && torchBehaviour.torchDetector.activeInHierarchy == true)
