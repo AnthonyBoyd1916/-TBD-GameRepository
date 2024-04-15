daf0fb729f1f52f3ad0344c07fc9600f12a1605c
NotTifako <sboyd7755@gmail.com>
2024-04-15 16:46:19 +0100

    going to try collider gates

--- Assets/S_DeathBehaviour.cs
+++ Assets/S_DeathBehaviour.cs
@@ -8,74 +8,21 @@ public class S_DeathBehaviour : MonoBehaviour
     [SerializeField] GameObject[] waypoint;
     [SerializeField] GameObject player;
 
-    int waypointIndex;
-    int waypointIndexTesting;
-    float distance;
-    float smallestDistance;
+    int mostRecentWp;
+    int playerMostRecentWp;
 
     private void Start()
     {
-        waypointIndex = 0;
+
     }
 
     private void FixedUpdate()
     {
         FindPlayer();
-        // transform.position = waypoint[waypointIndex].transform.position;
-
-        transform.position = Vector2.MoveTowards(this.transform.position, waypoint[waypointIndex].transform.position, Time.fixedDeltaTime);
     }
 
     void FindPlayer()
     {
-        waypointIndexTesting = 0;
-        //distance = Vector2.Distance(waypoint[0].transform.position, player.transform.position);
-        //smallestDistance = distance;
-        //distance = Vector2.Distance(waypoint[1].transform.position, player.transform.position);
-
-        //if (distance < smallestDistance)
-        //{
-        //    smallestDistance = distance;
-        //    waypointIndex = 1;
-        //}
-        //else
-        //{
-        //    waypointIndex = 0;
-        //}
-
-        //distance = Vector2.Distance(waypoint[2].transform.position, player.transform.position);
-
-        //if (distance < smallestDistance)
-        //{
-        //    smallestDistance = distance;
-        //    waypointIndex = 2;
-        //}
-
-        //distance = Vector2.Distance(waypoint[3].transform.position, player.transform.position);
-
-        //if(distance < smallestDistance)
-        //{
-        //    smallestDistance = distance;
-        //    waypointIndex = 3;
-        //}
-
-        while (waypointIndexTesting < waypoint.Length)
-        {
-            distance = Vector2.Distance(waypoint[waypointIndexTesting].transform.position, player.transform.position);
-            smallestDistance = distance;
-
-            if (waypointIndexTesting + 1 <= waypoint.Length)
-            {
-                distance = Vector2.Distance(waypoint[waypointIndexTesting + 1].transform.position, player.transform.position);
-            }
-
-            if (distance < smallestDistance)
-            {
-                smallestDistance = distance;
-                waypointIndex = waypointIndexTesting;
-            }
-
-            waypointIndex++;
-        }
+        
     }
 }
