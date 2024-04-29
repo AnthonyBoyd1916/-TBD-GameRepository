9b172b241bb7b14a7a866dc9e3adffebd2d66313
Jammie4444 <Wright-J31@ulster.ac.uk>
2024-04-29 12:45:52 +0100

    AudioManager

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,10 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] musicSounds, sfxSounds;
     public AudioSource musicSource, sfxSource;
+
+    public void PlayMusic(string Name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.Name == Name);
+
+        if (s != null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string Name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.Name == Name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
 
 }
